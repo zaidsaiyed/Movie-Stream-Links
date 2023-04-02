71c42308b35a757ae88757212dce566d862855f0
Zaid Saiyed <zaid.akil.saiyed@gmail.com>
2023-04-02 04:12:56 -0400

    Got all provider information.

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from justwatch import JustWatch
 import json
 
-    
-just_watch = JustWatch(country='CA')
+just_watch = JustWatch(country='US')
 user_input = input("Enter the movie name: ")
 
-results = just_watch.search_for_item(query = user_input)
+results = just_watch.search_for_item(query=user_input)
 pro = just_watch.get_providers()
-
 
 for i in results.get('items'):
     if i.get('offers') is None:
         continue
     print(i.get('title'))
-    off = (i.get('offers'))
-    print(off[0].get('urls').get('standard_web'))
-    print("----"*26)
+    for offer in i.get('offers'):
+        urls = offer.get('urls')
+        if urls is None:
+            continue
+        for url_type, url in urls.items():
+            print(f"{offer.get('provider_id')} - {offer.get('monetization_type')}: {url}")
+    print("----" * 26)
+
 with open('config.json', "w") as f:
     json.dump(results, f, indent=4)
